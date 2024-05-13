f30159a24463aca4601600914743b6368106b832
Mohammed Ali <mohammedaalli088@gmail.com>
2024-05-13 04:44:00 +0000

    feat: working fine now, on test

--- payStack/admin.py
+++ payStack/admin.py
@@ -3,4 +3,7 @@ from payStack.models import Payment
 
 # Register your models here.
 
-admin.site.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['amount','email','ref','verified', 'date_created']
+
+admin.site.register(Payment,PaymentAdmin)
